a1e88e7d8a1dcbbfa6d2af4d78ffef13c5170023
Haugholt <haugholt@gmail.com>
2012-01-24 13:50:40 +0100

    updated spec script to run correct specification

--- spec.bat
+++ spec.bat
@@ -5,7 +5,7 @@ echo ===========
 echo.
 REM src\MoneyOverview\packages\Machine.Specifications.0.5.2.0\tools\mspec src\MoneyOverview\MoneyOverview.Core\bin\Debug\MoneyOverview.Core.dll 
 echo mspec-clr4.exe src\MoneyOverview\MoneyOverview.Core\bin\Debug\MoneyOverview.Core.dll %*
-MoneyOverview\packages\Machine.Specifications.0.5.2.0\tools\mspec-clr4.exe %* MoneyOverview\MoneyOverview.Core\bin\Debug\MoneyOverview.Core.dll 
+MoneyOverview\packages\Machine.Specifications.0.5.2.0\tools\mspec-clr4.exe %* MoneyOverview\MoneyOverview.Core.Specifications\bin\Debug\MoneyOverview.Core.Specifications.dll 
 
 echo.
 echo.
@@ -13,4 +13,4 @@ echo Consider html report:
 echo --html \ego\@ferdig\mspec.html
 
 :end
-echo.
+echo
