b8b5bdb2ebfbcbfd6210b993a43eb9f3bd97e8b4
Haugholt <haugholt@gmail.com>
2012-01-24 14:09:39 +0100

    Removed mspec references from core

--- MoneyOverview/MoneyOverview.Core.Specifications/Class1.cs
+++ MoneyOverview/MoneyOverview.Core.Specifications/Class1.cs
@@ -11,23 +11,56 @@ namespace MoneyOverview.Core.Specifications
     {
         //static Dummy invoice;
 
-        Establish the_context = () =>
-        {
-            //List<string> l = new List<string>();
-            //l.Add("test");
+        Establish the_context;
+        //Establish the_context = () =>
+        //{
+        //    //List<string> l = new List<string>();
+        //    //l.Add("test");
 
-        };
+        //};
 
+        Because of;
         //Because of = () =>
         //    invoice = new Dummy();
 
-        It has_no_sales_tax_added;
+        It should_report_sum_of_zero;
         //= () =>
         //    invoice.ShouldBeOfType<Dummy>();
+
+        //Not implemented:
+        It should_list_zero_transactions;
+    }
+
+
+
+
+    /*
+    [Subject(typeof(Dummy))]
+    class Class1
+    {
+        static Dummy invoice ;
+
+        Establish the_context = () =>
+        {
+            List<string> l = new List<string>();
+            l.Add("test");
+
+        };
+
+        Because of = () => 
+            invoice = new Dummy();
+
+        It has_no_sales_tax_added = () =>
+            invoice.ShouldBeOfType<Dummy>();
 
         //Not implemented:
         It should_default_published_date_to_now;
     }
 
+    //TODO: Should not live in the test project :)
+    class Dummy {
+        public Dummy() { }
+    } 
+*/
     
 }
--- spec.bat
+++ spec.bat
@@ -1,13 +1,19 @@
 @echo off
+
+
 echo.
 echo MSpec Tests
 echo ===========
 echo.
+echo *** Build:
+c:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe .\MoneyOverview\MoneyOverview.sln /v:m
 REM src\MoneyOverview\packages\Machine.Specifications.0.5.2.0\tools\mspec src\MoneyOverview\MoneyOverview.Core\bin\Debug\MoneyOverview.Core.dll 
+echo.
+echo *** Spec:
 echo mspec-clr4.exe src\MoneyOverview\MoneyOverview.Core\bin\Debug\MoneyOverview.Core.dll %*
 MoneyOverview\packages\Machine.Specifications.0.5.2.0\tools\mspec-clr4.exe %* MoneyOverview\MoneyOverview.Core.Specifications\bin\Debug\MoneyOverview.Core.Specifications.dll 
-
 echo.
+
 echo.
 echo Consider html report:
 echo --html \ego\@ferdig\mspec.html
