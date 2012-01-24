d6da15c30fe65a18f1752f1cbf4af7a7e9d58f5a
Haugholt <haugholt@gmail.com>
2012-01-24 13:47:03 +0100

    added specs to the correct project

--- MoneyOverview/MoneyOverview.Core.Specifications/Class1.cs
+++ MoneyOverview/MoneyOverview.Core.Specifications/Class1.cs
@@ -2,10 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Machine.Specifications;
 
 namespace MoneyOverview.Core.Specifications
 {
-    public class Class1
+    [Subject("IN")]
+    class When_Income_Is_Not_Registered
     {
+        //static Dummy invoice;
+
+        Establish the_context = () =>
+        {
+            //List<string> l = new List<string>();
+            //l.Add("test");
+
+        };
+
+        //Because of = () =>
+        //    invoice = new Dummy();
+
+        It has_no_sales_tax_added;
+        //= () =>
+        //    invoice.ShouldBeOfType<Dummy>();
+
+        //Not implemented:
+        It should_default_published_date_to_now;
     }
+
+    
 }
