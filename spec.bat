5d7ab3e6fd5d777cc887b6051a9f3738b2f9f328
Haugholt <haugholt@gmail.com>
2012-01-24 13:09:28 +0100

    added spec.bat

---
+++ spec.bat
@@ -0,0 +1,16 @@
+@echo off
+echo.
+echo MSpec Tests
+echo ===========
+echo.
+REM src\MoneyOverview\packages\Machine.Specifications.0.5.2.0\tools\mspec src\MoneyOverview\MoneyOverview.Core\bin\Debug\MoneyOverview.Core.dll 
+echo mspec-clr4.exe src\MoneyOverview\MoneyOverview.Core\bin\Debug\MoneyOverview.Core.dll %*
+MoneyOverview\packages\Machine.Specifications.0.5.2.0\tools\mspec-clr4.exe %* MoneyOverview\MoneyOverview.Core\bin\Debug\MoneyOverview.Core.dll 
+
+echo.
+echo.
+echo Consider html report:
+echo --html \ego\@ferdig\mspec.html
+
+:end
+echo.
