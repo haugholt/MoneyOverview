c0ece36da86b9cb05e35e808d6bd89a9051d3c1d
Haugholt <haugholt@gmail.com>
2012-01-24 12:55:48 +0100

    Initial spike for MSpec

---
+++ MoneyOverview/MoneyOverview.Core/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MoneyOverview.Core
+{
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //    }
+    //}
+}
