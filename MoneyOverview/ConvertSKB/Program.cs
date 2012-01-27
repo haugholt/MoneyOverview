bd899b55962ed8681e81a040495559a3e7e2114a
Haugholt <haugholt@gmail.com>
2012-01-27 10:11:26 +0100

    Added empty commandline project for converting SKB files

---
+++ MoneyOverview/ConvertSKB/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConvertSKB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Usage();
+                return;
+            }
+            
+            string path = args[0];
+            Console.Out.WriteLine("Path: "+path);
+        }
+
+        private static void Usage()
+        {
+            Console.Out.WriteLine("Provide a path");
+            
+        }
+    }
+}
