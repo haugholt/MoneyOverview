c5a177486fec05ced5b878dd2b585d3849f73cc9
Haugholt <haugholt@gmail.com>
2012-02-13 13:35:56 +0100

    Add more generic rinse for description

--- MoneyOverview/ConvertSKB/Domain/Payees/PayeeExtractor.cs
+++ MoneyOverview/ConvertSKB/Domain/Payees/PayeeExtractor.cs
@@ -25,25 +25,20 @@ namespace ConvertSKB.Domain.Payees
 
         private void StripNonRepeatingData()
         {
-            /*
-*1065 20.12 NOK 200,00 BUYPASS MINIBAN
-*1065 20.12 GBP 28,82 Amazon EU KURS: 9,3948
-rjan Haugholt
-*1065 05.12 GBP 42,50 WWW.EMMABRIDGEWATE KURS: 9,1788
-*1065 30.11 NOK 275,00 ONE.COM
-*1065 02.09 NOK 2074,00 RENT-A-WRECK OSLO VEST
-*1065 04.09 SEK 217,00 RASTA SVINESUND KURS: 0,8581
-*1065 04.09 SEK 50,00 RASTA SVINESUND KURS: 0,8582
-*1065 04.09 SEK 1931,80 NORDBY SUPERMARKET KURS: 0,8581
-*1065 04.09 SEK 858,00 BABYPROFFSEN KURS: 0,8594
-*1065 04.09 SEK 845,00 HAPPYLAND BABYSHOP KURS: 0,8594
-Fra: rjan Haugholt Betalt: 24.11.11
-Kostnader SMS-bank Juli 2011
-             
-             * */
             StripNonRepeatingData("^[0-9]{2}\\.[0-9]{2}");
+            
             StripNonRepeatingData("^TIL: *");
             StripNonRepeatingData(" *BETNR: *[0-9]{1,5}$");
+
+            StripNonRepeatingData("^\\*[0-9]{4} [0-9]{2}\\.[0-9]{2} (NOK|GBP|SEK) [0-9]*,[0-9]{2} ");
+            StripNonRepeatingData(" KURS: [0-9]*,[0-9]*$");
+            
+            StripNonRepeatingData("^Fra: ");
+            StripNonRepeatingData(" Betalt: [0-9]{2}\\.[0-9]{2}\\.[0-9]{2}");
+            StripNonRepeatingData(" (Jan|Feb|Mar|Apr|Mai|Jun|Jul|Aug|Sep|Okt|Nov|Des|May|Oct|Dec) ");
+            StripNonRepeatingData(" (Januar|Februar|Mars|April|Mai|Juni|Juli|August|September|Oktober|November|Desember) ");
+            StripNonRepeatingData("2011");
+            StripNonRepeatingData("  *");
         }
 
         private void StripNonRepeatingData(string regexstring) {
