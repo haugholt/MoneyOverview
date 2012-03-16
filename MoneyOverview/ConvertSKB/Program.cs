76e59a6c04ea21d62bb18c37401fd19b5331f74b
Haugholt <haugholt@gmail.com>
2012-03-16 15:35:16 +0100

    Embed core as resource

--- MoneyOverview/ConvertSKB/Program.cs
+++ MoneyOverview/ConvertSKB/Program.cs
@@ -7,8 +7,27 @@ namespace ConvertSKB
 {
     class Program
     {
+        static System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+
+            string dllName = args.Name.Contains(',') ? args.Name.Substring(0, args.Name.IndexOf(',')) : args.Name.Replace(".dll", "");
+
+            dllName = dllName.Replace(".", "_");
+
+            if (dllName.EndsWith("_resources")) return null;
+
+            System.Resources.ResourceManager rm = new System.Resources.ResourceManager(typeof(Program).Namespace + ".Properties.Resources", System.Reflection.Assembly.GetExecutingAssembly());
+
+            byte[] bytes = (byte[])rm.GetObject(dllName);
+
+            return System.Reflection.Assembly.Load(bytes);
+
+        }
+
         static void Main(string[] args)
         {
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDomain_AssemblyResolve);
+
             if (args.Length != 2)
             {
                 Usage();
